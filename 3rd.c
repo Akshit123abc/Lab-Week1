9c8ec7d13184a920ebc5da924028f16c83197a08
Akshit123abc <22104041@mail.jiit.ac.in>
2024-08-14 04:31:22 +0000

    answer 2 and 3

--- 3rd.c
+++ 3rd.c
@@ -1,13 +1,58 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
-transpose(original, transposed, rows, cols);
+#define MAX_WORDS 1000
+#define MAX_WORD_LENGTH 50
 
-printf("The transpose of the matrix is:\n");
-for (int i = 0; i < cols; i++) {
-for (int j = 0; j < rows; j++) {
-printf("%d ", transposed[i][j]);
+struct Word {
+char text[MAX_WORD_LENGTH];
+int frequency;
+};
+
+void toLowerCase(char *str) {
+for (int i = 0; str[i]; i++) {
+str[i] = tolower(str[i]);
 }
-printf("\n");
+}
+
+int main()
+{ while (token != NULL) {
+toLowerCase(token);
+
+int main() {
+char paragraph[10000];
+struct Word words[MAX_WORDS];
+int wordCount = 0;
+
+printf("Enter a paragraph: ");
+fgets(paragraph, sizeof(paragraph), stdin);
+
+char *token = strtok(paragraph, " \t\n.,;:!?\"()");
+while (token != NULL) {
+toLowerCase(token);
+
+int found = 0;
+for (int i = 0; i < wordCount; i++) {
+if (strcmp(words[i].text, token) == 0) {
+words[i].frequency++;
+found = 1;
+break;
+}
+}
+
+if (!found && wordCount < MAX_WORDS) {
+strcpy(words[wordCount].text, token);
+words[wordCount].frequency = 1;
+wordCount++;
+}
+
+token = strtok(NULL, " \t\n.,;:!?\"()");
+}
+
+printf("\nWord frequencies:\n");
+for (int i = 0; i < wordCount; i++) {
+printf("%s: %d\n", words[i].text, words[i].frequency);
 }
 
 return 0;
-}
